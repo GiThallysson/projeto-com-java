9b3f4d1e13d1278c91f48c033f52d2340e7f67aa
GiThallysson <112260975+GiThallysson@users.noreply.github.com>
2023-10-25 23:46:22 -0300

    Update MainAplicaPessoa.java

--- Tarefa/MainAplicaPessoa.java
+++ Tarefa/MainAplicaPessoa.java
@@ -2,13 +2,6 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-
-
-
-
-import java.util.Date;
-import java.util.List;
-
 class Pessoa {
     private String nome;
     private Date dataNascimento;
