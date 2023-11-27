0c687b4474811cafa79449d1a27937feff5e8bfd
GiThallysson <112260975+GiThallysson@users.noreply.github.com>
2023-11-27 15:17:11 -0300

    Create EstadoConcretoAnimado.java

---
+++ Trabalho/grupopadrão/EstadoConcretoAnimado.java
@@ -0,0 +1,5 @@
+class EstadoConcretoAnimado implements Estado {
+    public void manipularSolicitação() {
+        System.out.println("EstadoConcretoAnimado lidou promulga com a solicitação.");
+    }
+}
